b26c6db9c1beca339fc5d1b533ac8d6277bb1802
Gyong <gyong@asceoft.com>
2024-04-28 10:53:02 +0800

    enable state for livestream status

--- src/modules/livestream/ui-components.tsx
+++ src/modules/livestream/ui-components.tsx
@@ -21,15 +21,15 @@ export function LiveStreamPreview() {
 }
 
 export function LiveStatusIndicator() {
-  // let ctx = LiveStreamContext; 
-  // useEffect(()=>console.log(ctx),[])
-  console.count('render time')
+  let ctx = LiveStreamContext;
+  useEffect(() => console.log(ctx), []);
+  console.count("render time");
   return (
     <div
-      // data-active={ctx.liveState.status === "live"}
+      data-active={ctx.liveState.status === "live"}
       className="data-[active=true]:bg-red-500 bg-slate-600 text-white px-5 rounded-full w-fit absolute top-3 left-3 uppercase"
     >
-      {/* {ctx.liveState.status} */}
+      {ctx.liveState.status}
     </div>
   );
 }
